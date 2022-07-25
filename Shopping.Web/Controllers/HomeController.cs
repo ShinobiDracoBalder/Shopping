1c7c9ccc1fdae2cf46d023590b5651f021fefbb5
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2022-07-25 09:49:14 -0500

    Changed Basic Home screen Products

--- Shopping.Web/Controllers/HomeController.cs
+++ Shopping.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Web.Data;
+using Shopping.Web.Data.Entities;
 using Shopping.Web.Models;
 using System.Diagnostics;
 
@@ -7,16 +10,48 @@ namespace Shopping.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _dataContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext dataContext)
         {
             _logger = logger;
+            _dataContext = dataContext;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Product>? products = await _dataContext.Products
+            .Include(p => p.ProductImages)
+            .Include(p => p.ProductCategories)
+            .OrderBy(p => p.Description)
+            .ToListAsync();
+            List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
+            int i = 1;
+            foreach (Product? product in products)
+            {
+                if (i == 1)
+                {
+                    productsHome.LastOrDefault().Product1 = product;
+                }
+                if (i == 2)
+                {
+                    productsHome.LastOrDefault().Product2 = product;
+                }
+                if (i == 3)
+                {
+                    productsHome.LastOrDefault().Product3 = product;
+                }
+                if (i == 4)
+                {
+                    productsHome.LastOrDefault().Product4 = product;
+                    productsHome.Add(new ProductsHomeViewModel());
+                    i = 0;
+                }
+                i++;
+            }
+            return View(productsHome);
         }
+
 
         public IActionResult Privacy()
         {
